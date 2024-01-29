0dcc8bedb67d7b9ad7e972a91031b86b7a27e34b
tristanleng <146465771+tristanleng@users.noreply.github.com>
2024-01-29 18:42:55 +0000

    removed filters

--- src/filters/index.tsx
+++ src/filters/index.tsx
@@ -1,21 +1,21 @@
 import type { Filter } from "../types";
-import sampleRoseColoredGlasses from "./samples/roseColoredGlasses";
-import sampleGrid from "./samples/grid";
-import sampleVignette from "./samples/vignette";
+//import sampleRoseColoredGlasses from "./samples/roseColoredGlasses";
+//import sampleGrid from "./samples/grid";
+//import sampleVignette from "./samples/vignette";
 import { Brightner } from "./brightned";
 import { inverted } from "./invert";
 import { Border } from "./_templateRowCol";
-import { Blur } from "./blur";
+//import { Blur } from "./blur";
 import { temperature } from "./temperature";
 
 const filters: Filter[] = [
-  Blur,
+  //Blur,
   temperature,
   inverted,
   Border,
   Brightner,
   sampleRoseColoredGlasses,
-  sampleGrid,
+  //sampleGrid,
   sampleVignette,
 ];
 
